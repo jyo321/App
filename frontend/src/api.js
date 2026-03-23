ab3b089c93af1827d61f00ae741e0ba885385a4d
Jyothi Swaroop Marri <jyothiswaroop.marri@pal.tech>
2026-03-23 14:54:36 +0530

    changing server.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,16 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL =process.env.REACT_APP_API_URL;
 
 export const getMessage = async () => {
-  const response = await fetch(`${API_URL}/api/message`);
-  return response.json();
+  try {
+    const response = await fetch(`${API_URL}/api/message`);
+
+    if (!response.ok) {
+      throw new Error("Backend not reachable");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    return { message: "Backend not connected ❌" };
+  }
 };
